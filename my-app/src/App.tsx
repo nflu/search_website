e50fce18e8686e5b74ebf9d52b9e543fd5e91080
nflu <nflugovoy@berkeley.edu>
2025-06-11 22:56:12 -0700

    removed unused code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,36 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-interface LogoProps_t {
-
-}
-
-const Logo: React.FC<LogoProps_t> = (props: LogoProps_t) => {
-  return (
-    <div style={{
-      display: 'flex', 
-      padding: '4px',
-      paddingRight: '16px',
-    }} >
-      <div style={{ 
-        display: "inline-block", 
-        padding: '8px', 
-        alignItems: 'center',
-      }}>
-        <div style={{display: 'flex', borderBottomColor: 'rgba(191, 0, 255, 0.701)',
-        borderBottomWidth: '2px',
-        borderBottomStyle: 'solid',}}>
-          <p style={{paddingRight: "4px", margin: 0, fontSize: 22, fontFamily: `Trebuchet MS`, textAlign: 'right'}}>
-            <span style={{color: 'rgba(191, 0, 255, 0.701)'}}>True</span> Searcher
-          </p>
-          <img src={logo} style={{marginTop: '-7px', color: 'rgba(191, 0, 255, 0.701)'}} alt="Logo" height="40px"/>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 const HomeButton: React.FC<EmptyProps_t> = () => {
   return (<div className='clicky' onClick={() => (document.location.href = '/')}>
   <div className="logo-container">
